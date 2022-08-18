140a229df7c212e98bd33901ece5c83929342480
Dmitry <69222296+flathead@users.noreply.github.com>
2022-08-18 18:03:05 +0300

    0.2.1 - modal windows

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,170 @@
+'use strict';
+
+// ANCHOR Functions
+function phonesModal() {
+	Swal.fire( {
+		showCloseButton: true,
+		showConfirmButton: false,
+		html: '<div class="phone-container"><div class="title">' +
+			'<img src="./assets/img/png/Телефон.png" alt="">' +
+			'<span>Телефоны</span>' +
+		'</div>' +
+		'<div class="phone">' +
+			'<p class="title">Администор центра:</p>' +
+			'<a href="tel:8 (909)-930-00-70" class="phone">8 (909)-930-00-70</a>' +
+			'<a href="tel:8 (916)-584-68-57" class="phone">8 (916)-584-68-57</a>' +
+			'<a href="tel:8 (495)-640-25-62" class="phone">8 (495)-640-25-62</a>' +
+		'</div>' +
+		'<div class="phone">' +
+			'<p class="title">Интернет-магазин:</p>' +
+			'<a href="tel:8 (909)-930-10-70" class="phone">8 (909)-930-10-70</a>' +
+		'</div>' +
+		'<div class="phone">' +
+			'<p class="title">Онлайн-школа:</p>' +
+			'<a href="tel:8 (909)-930-10-70" class="phone">8 (909)-930-10-70</a>' +
+		'</div>' +
+		'<div class="open">' +
+			'<p class="title">График работы:</p>' +
+			'<p>Ежедневно с 10:00 - 19:00</p>' +
+		'</div></div>',
+	} );
+}
+
+function forgotModal() {
+	Swal.fire( {
+		showCloseButton: true,
+		showConfirmButton: false,
+		html: '<div class="forgot-container">' +
+			'<div class="title">' +
+				'<p>Восстановление пароля</p>' +
+			'</div>' +
+			'<div class="form">' +
+				'<form class="auth-register" id="forgot-password" method="POST" action="/Celius-Static/account">' +
+					'<label for="email">Введите ваш Email <span class="needs">*</span></label>' +
+					'<input type="text" id="email" placeholder="Введите Email" />' +
+					'<input class="action-btn" type="submit" id="remind-password" value="Отправить письмо" />' +
+				'</form>' +
+			'</div>' +
+		'</div>',
+	} );
+
+	$( 'input#remind-password' ).on( 'click', function() {
+		Swal.fire( {
+			showCloseButton: false,
+			showConfirmButton: true,
+			icon: 'success',
+			title: 'Отлично!',
+			text: 'Мы отправили письмо с информацией о восстановлении пароля на указанный Вами Email',
+			confirmButtonText: 'Хорошо',
+			/* timer: 3000, */
+		} );
+	} )
+}
+
+function authModal() {
+	Swal.fire( {
+		showCloseButton: true,
+		showConfirmButton: false,
+		html: '<div class="auth-container"><div class="title">' +
+			'<p>Вход в личный кабинет</p>' +
+		'</div>' +
+		'<div class="break">' +
+			'<p>через</p>' +
+		'</div>' +
+		'<div class="social">' +
+			'<a href="#"><img src="../assets/img/svg/socials/Яндекс.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Google.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Мейл.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Вконтакте.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Одноклассники.svg" alt="" /></a>' +
+		'</div>' +
+		'<div class="break">' +
+			'<p>или</p>' +
+		'</div>' +
+		'<div class="form">' +
+			'<form method="POST" action="/Celius-Static/account" id="auth-form" class="auth-register">' +
+				'<label for="login">Логин <span class="needs">*</span></label>' +
+				'<input type="text" name="login" id="login" placeholder="Введите Email" />' +
+				'<label for="password">Пароль <span class="needs">*</span></label>' +
+				'<input type="password" name="password" id="password" placeholder="Введите пароль" />' +
+				'<button id="forgot">Забыли пароль?</button>' +
+				'<input class="action-btn" type="submit" value="Войти">' +
+			'</form>' +
+			'<p class="question">Ещё нет аккаунта? <button type="button" id="register">Зарегистрируйтесь</button></p>' +
+		'</div></div>',
+	} );
+
+	// Если нажата кнопка Регистрации - открыть окно регистрации
+	$( 'button#register' ).on( 'click', registerModal );
+	// Если нажата кнопка "Забыли пароль?" - открыть окно восстановления пароля
+	$( 'button#forgot' ).on( 'click', forgotModal );
+}
+
+function registerModal() {
+	Swal.fire( {
+		showCloseButton: true,
+		showConfirmButton: false,
+		html: '<div class="auth-container"><div class="title">' +
+			'<p>Регистрация на сайте</p>' +
+		'</div>' +
+		'<div class="break">' +
+			'<p>через</p>' +
+		'</div>' +
+		'<div class="social">' +
+			'<a href="#"><img src="../assets/img/svg/socials/Яндекс.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Google.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Мейл.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Вконтакте.svg" alt="" /></a>' +
+			'<a href="#"><img src="../assets/img/svg/socials/Одноклассники.svg" alt="" /></a>' +
+		'</div>' +
+		'<div class="break">' +
+			'<p>или</p>' +
+		'</div>' +
+		'<div class="form">' +
+			'<form method="POST" action="/Celius-Static/account" id="reg-form" class="auth-register">' +
+				'<label for="login">Укажите Ваш Email <span class="needs">*</span></label>' +
+				'<input type="text" name="login" id="login" placeholder="Введите Email" />' +
+				'<div class="grid">' +
+					'<div>' +
+						'<label for="firstname">Имя <span class="needs">*</span></label>' +
+						'<input type="text" name="firstname" id="firstname" placeholder="Введите Имя" />' +
+					'</div>' +
+					'<div>' +
+						'<label for="secondname">Фамилия <span class="needs">*</span></label>' +
+						'<input type="text" name="secondname" id="secondname" placeholder="Ваша Фамилия" />' +
+					'</div>' +
+				'</div>' +
+				'<label for="password">Придумайте Пароль <span class="needs">*</span></label>' +
+				'<input type="password" name="password" id="password" placeholder="Рекомендуем от 6 символов" />' +
+				'<button id="forgot">Забыли пароль?</button>' +
+				'<input class="action-btn" type="submit" value="Зарегистрироваться" />' +
+				'<p class="warning">Нажимая на кнопку, я соглашаюсь на обработку <a href="/Celius-Static/maintenance" target="_blank">персональных данных</a></p>' +
+			'</form>' +
+			'<p class="question">Ещё нет аккаунта? <button type="button" id="auth">Войдите</button></p>' +
+		'</div></div>',
+	} );
+
+	// Если нажата кнопка Авторизации - открыть окно авторизации
+	$( 'button#auth' ).on( 'click', authModal );
+	// Если нажата кнопка "Забыли пароль?" - открыть окно восстановления пароля
+	$( 'button#forgot' ).on( 'click', forgotModal );
+}
+
+// ANCHOR DOM Ready
+
 jQuery( function( $ ) {
+	/* [START]
+	 * Модальное окно для телефонов
+	 */
+	$( 'span.show-phones' ).on( 'click', phonesModal );
+	$( 'button#auth' ).on( 'click', authModal );
+	$( 'button#register' ).on( 'click', registerModal );
+	/* [END]
+	 * Модальное окно для телефонов
+	 */
+
+//	[][][][][][][][][][][][][][][][][][][][][][][][][][][][]
+
 	/* [START]
 	 * Анимация поля поиска при форкусировке на нём
 	 */
@@ -35,7 +201,7 @@ jQuery( function( $ ) {
 	$( 'header.site-header.mobile ul.header-menu li.menu-item.has-child a' ).on( 'click', function() {
 		$( this ).toggleClass( 'active' );
 		if (touchtime == 0) {
-			// set first click
+			// первый клик
 			touchtime = new Date().getTime();
 			if( $( this ).hasClass( 'active' ) ) {
 				$( this ).closest( '.has-child' ).find( 'ul.sub-menu' ).slideDown( 'slow' );
@@ -46,14 +212,14 @@ jQuery( function( $ ) {
 			return false;
 		} else {
 			if (((new Date().getTime()) - touchtime) < 800) {
-				// double click
+				// двойной клик
 				
 				window.location = this.href;
 				touchtime = 0;
         		return false;
 				
 			} else {
-				// not a double click
+				// не двойной и не первый клики
 				touchtime = new Date().getTime();
 
 				if( $( this ).hasClass( 'active' ) ) {
